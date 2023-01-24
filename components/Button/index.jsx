cfca692575ed6dd4db3f50d26c9f80364df66bc8
MadiMagpie <mcafikirwin@gmail.com>
2023-01-23 22:16:57 -0800

    sets up simple weather app

---
+++ components/Button/index.jsx
@@ -0,0 +1,31 @@
+import styled from 'styled-components';
+
+const StyledButton = styled.button`
+border: none;
+outline: none;
+border-radius: 0.5em;
+background-color: ${props => props.backgroundColor || '#998f88'};
+color: ${props => props.color || '#FFFFFF'};
+padding: ${props => props.padding || '0.75em 1.25em'};
+width: ${props => props.size === 'short' ? '5rem' : props.size === 'long' ? '25em' : '10rem'};
+min-width: fit-content;
+font-family: 'Heebo', sans-serif;
+font-weight: 600;
+margin : ${props => props.margin || '0 0 0 0'};
+
+&:hover {
+    cursor: pointer;
+    background-color: ${props => props.hoverColor || '#7A211D'};
+}
+`;
+
+export const Button = ({ length, bgColor, margin, padding, colour, label, ...props }) => {
+    return (
+        <StyledButton
+            type = "submit"
+            {...props}
+        >
+            {label}
+        </StyledButton>
+    );
+};
